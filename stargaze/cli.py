6bb4163410798405222a2694c00bba8d745e4719
Alaksiej Kanstancinau <konstantinov.a.y2005@gmail.com>
2025-01-01 17:38:03 +0100

    Add __main__, cli and core modules, define requirements.

---
+++ stargaze/cli.py
@@ -0,0 +1,17 @@
+"""Command-line interface."""
+
+# stargaze --near whereabouts --within radius --head direction
+# for example:
+# stargaze --near Krakow --within 30km --head south
+# `within` argument should understand units of length, `pint` module can be used
+# for conversion
+# `head` argument should understand textual directions such as south, west,
+# north, easth, s, w, n, e, sw, etc. and numeric azimuth
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ stargaze/core.py
@@ -0,0 +1,6 @@
+"""Core application logic."""
+
+# this module should contain the following functionality:
+# - identify missing area (return a list of bounding boxes probably)
+# - run importers if needed
+# - run search
