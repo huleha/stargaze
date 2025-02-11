1307d57540a66a7d66941d89d4505b465a7de608
lisikava <mlisikava@gmail.com>
2025-02-11 20:05:05 +0100

    Add web app

---
+++ stargaze/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from stargaze.geocoding import find_coordinates
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        whereabouts = request.form.get('whereabouts')
+        coordinates = find_coordinates(whereabouts)
+        return render_template('index.html', coordinates=coordinates)
+    return render_template('index.html', coordinates=None)
