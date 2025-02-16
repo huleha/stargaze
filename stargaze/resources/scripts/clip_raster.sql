812f5999c92d2c660463fc05be06d72ce9ae54b0
nhuleha <huleha@student.agh.edu.pl>
2025-02-16 16:43:38 +0100

    Fix CardinalityViolation bug in raster data import

--- stargaze/resources/scripts/clip_raster.sql
+++ stargaze/resources/scripts/clip_raster.sql
@@ -1,25 +1,26 @@
 with params as (
     select
-        st_envelope(relief.rast) as envelope
+        ST_Envelope(relief.rast) as envelope
     from
         relief),
 grid as (
     select
-        st_geohash(tile,5) as geohash,
-        box2d(st_envelope(tile)) as bbox,
+        ST_Geohash(ST_Centroid(tile),5) as geohash,
+        Box2D(ST_Envelope(tile)) as bbox,
         grid.tile as tile
     from
         params,
-        st_squaregrid(180/(2^12), params.envelope) as grid(tile))
+        ST_SquareGrid(180/(2^12), params.envelope) as grid(tile))
 insert into relief_tiles(geohash, bbox, tile)
 select
     grid.geohash,
     grid.bbox,
-    st_clip(relief.rast, grid.tile) as tile
+    ST_Clip(relief.rast, grid.tile)
 from
     relief,
     grid
 on conflict (geohash) do update
-set tile = excluded.tile;
+set tile = excluded.tile
+;
 
 delete from relief;
