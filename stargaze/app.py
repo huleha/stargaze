ac0d62645f3620be09f6acd1c93245d148a213a6
lisikava <mlisikava@gmail.com>
2025-02-16 22:28:24 +0100

    Update web app

--- stargaze/app.py
+++ stargaze/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from stargaze.geocoding import find_coordinates
+from stargaze.cli import parse_length, parse_direction
+from stargaze.core import stargaze
 
 app = Flask(__name__)
 
@@ -8,13 +10,30 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         try:
-            whereabouts = request.form.get('whereabouts')
-            if whereabouts:
-                coordinates = find_coordinates(whereabouts)
-                return render_template('index.html', coordinates=coordinates, whereabouts=whereabouts)
+            input_whereabouts = request.form.get('whereabouts')
+            if input_whereabouts:
+                coordinates = find_coordinates(input_whereabouts)
+                input_radius = request.form.get('radius')
+                input_direction = request.form.get('direction', '0.0')
+                direction = parse_direction(input_direction)
+                print("AAAAAAAAAAAAA", direction)
+                if input_radius:
+                    print("bbb", direction)
+                    radius = parse_length(input_radius)
+                    spots = stargaze(coordinates, radius, direction)
+
+                    return render_template('index.html', whereabouts=input_whereabouts, radius=input_radius,
+                                           spots=spots, direction=direction)
+                print("ccc", direction)
+                radius = 3000
+                spots = stargaze(coordinates, radius, direction)
+                return render_template('index.html', whereabouts=input_whereabouts, radius=radius,
+                                       spots=spots, direction=direction)
+
         except IndexError:
             return render_template('index.html', coordinates=None)
     return render_template('index.html', coordinates=None)
+
 
 @app.route('/about')
 def about():
