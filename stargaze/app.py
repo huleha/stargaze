a6b77373e0779790ff53bdf289f63284a841150c
lisikava <mlisikava@gmail.com>
2025-02-17 00:30:31 +0100

    Update web app

--- stargaze/app.py
+++ stargaze/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from stargaze.geocoding import find_coordinates
 from stargaze.cli import parse_length, parse_direction
 from stargaze.core import stargaze
+from stargaze.sessions import SessionFactory
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         try:
+            call_function = True
             input_whereabouts = request.form.get('whereabouts')
+
             if input_whereabouts:
                 coordinates = find_coordinates(input_whereabouts)
                 input_radius = request.form.get('radius')
@@ -19,14 +22,14 @@ def index():
                 if input_radius:
                     radius = parse_length(input_radius)
                     spots = stargaze(coordinates, radius, direction)
-
+                    # SessionFactory.get_instance().close()
                     return render_template('index.html', whereabouts=input_whereabouts, radius=input_radius,
-                                           spots=spots, direction=direction)
+                                           spots=spots, direction=direction, call_function=call_function)
                 radius = 3000
                 spots = stargaze(coordinates, radius, direction)
+                # SessionFactory.get_instance().close()
                 return render_template('index.html', whereabouts=input_whereabouts, radius=radius,
-                                       spots=spots, direction=direction)
-
+                                       spots=spots, direction=direction, call_function=call_function)
         except IndexError:
             return render_template('index.html', coordinates=None)
     return render_template('index.html', coordinates=None)
